e6db497b94b40c810c036eb27d74c81582560d69
Aziz <example@.com>
2022-12-22 23:54:59 +0500

    task1

---
+++ main.js
@@ -0,0 +1,17 @@
+var word = prompt('Введите ваше имя')
+console.log('Меня зовут '+ word);
+
+var age = +prompt('Ваш возраст')
+console.log('Мне '+ age);
+
+var task = +prompt('Решите пример. Сколько будет 3+9')
+console.log ('Пример 1: 3+9 = 12 Ваш ответ ' + task );
+
+var task = +prompt('Решите пример. Сколько будет 9-3')
+console.log ('Пример 1: 9-3 = 6 Ваш ответ ' + task );
+
+var task = +prompt('Решите пример. Сколько будет 3+9')
+console.log ('Пример 1: 52*0 = 0 Ваш ответ ' + task );
+
+var congr = +prompt('Поздравляю задание пройдено')
+console.log ('Вы старались');
